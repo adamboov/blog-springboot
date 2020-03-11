7235a8ea4e800eb6c83ad0147fa2a86ef26251c0
adamboov <adamboov@163.com>
2020-03-11 11:17:30 +0800

    日志切面类编写

--- src/test/java/com/adam/blog/BlogApplicationTests.java
+++ src/test/java/com/adam/blog/BlogApplicationTests.java
@@ -1,12 +1,12 @@
 package com.adam.blog;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogApplicationTests {
 
-//    @Test
+    @Test
     void contextLoads() {
     }
 
