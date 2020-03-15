e22bfb1907f0d0a01c0fe8d7213aae6dd2f6c71d
adamboov <adamboov@163.com>
2020-03-15 23:26:06 +0800

    TypeController博客分类路由编写 TypeRepository 分类JPA接口定义 TypeService TypeServiceImpl 分类service实现 部分HTML模板改进

---
+++ src/main/java/com/adam/blog/dao/TypeRepository.java
@@ -0,0 +1,9 @@
+package com.adam.blog.dao;
+
+import com.adam.blog.entity.Type;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeRepository extends JpaRepository<Type, Long> {
+
+
+}
---
+++ src/main/java/com/adam/blog/web/admin/TypeController.java
@@ -0,0 +1,45 @@
+package com.adam.blog.web.admin;
+
+import com.adam.blog.entity.Type;
+import com.adam.blog.service.TypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class TypeController {
+
+    @Autowired
+    private TypeService typeService;
+
+    @GetMapping("/types")
+    public String types(@PageableDefault(size = 10, sort = {"id"}, direction = Sort.Direction.DESC)
+                                    Pageable pageable, Model model){
+        model.addAttribute("page", typeService.listType(pageable));
+        typeService.listType(pageable);
+        return "admin/types";
+    }
+
+    @GetMapping("/types/input")
+    public String input(){
+        return "admin/type-input";
+    }
+
+    @PostMapping("/types")
+    public String post(Type type){
+        Type t = typeService.saveType(type);
+        if (t == null){
+
+        } else {
+
+        }
+        return "redirect:/admin/types";
+    }
+}
