9ec66f39d5e4575ed868037a788f2b49e82df804
adamboov <adamboov@163.com>
2020-03-15 12:00:32 +0800

    LoginInterceptor博客管理拦截器编写 WebConfig 拦截器配置类编写 BlogController -> list() admin下的博客路由方法

---
+++ src/main/java/com/adam/blog/interceptor/LoginInterceptor.java
@@ -0,0 +1,23 @@
+package com.adam.blog.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**拦截器
+ * 未登录者不可查看管理
+ */
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+        if (request.getSession().getAttribute("user") == null){
+            response.sendRedirect("/admin");
+            return false;
+        }
+        return true;
+    }
+}
