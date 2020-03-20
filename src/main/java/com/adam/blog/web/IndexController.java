b78fe6d1c909d19ba6e4dae68fdeb44fc1a08d4a
adamboov <48423629+adamboov@users.noreply.github.com>
2020-03-20 20:45:11 +0800

    搜索功能实现

--- src/main/java/com/adam/blog/web/IndexController.java
+++ src/main/java/com/adam/blog/web/IndexController.java
@@ -1,6 +1,5 @@
 package com.adam.blog.web;
 
-import com.adam.blog.NotFoundException;
 import com.adam.blog.service.BlogService;
 import com.adam.blog.service.TagService;
 import com.adam.blog.service.TypeService;
@@ -11,7 +10,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -26,12 +26,21 @@ public class IndexController {
     private TagService tagService;
 
     @GetMapping({"/","/index","index.html"})
-    public String index(@PageableDefault(size = 10, sort = {"updateTime"}, direction = Sort.Direction.DESC)Pageable pageable, Model model){
+    public String index(@PageableDefault(size = 10, sort = {"updateTime"}, direction = Sort.Direction.DESC)Pageable pageable,
+                        Model model){
         model.addAttribute("page", blogService.listBlog(pageable));
         model.addAttribute("types", typeService.listTypeTop(10));
         model.addAttribute("tags", tagService.listTagTop(10));
         model.addAttribute("recommendBlogs", blogService.listRecommendBlogTop(10));
         return "index";
+    }
+
+    @PostMapping("/search")
+    public String search(@PageableDefault(size = 10, sort = {"updateTime"},direction = Sort.Direction.DESC)Pageable pageable,
+                         @RequestParam String query, Model model){
+        model.addAttribute("page", blogService.listBlog("%" + query + "%", pageable));
+        model.addAttribute("query", query);
+        return "search";
     }
 
     @GetMapping("/blog/{id}")
