a63584c69c60afaa4692c18d6d9b0238c055c141
adamboov <48423629+adamboov@users.noreply.github.com>
2020-03-24 13:08:40 +0800

    500 error 模板修复

--- src/main/java/com/adam/blog/service/CommentServiceImp.java
+++ src/main/java/com/adam/blog/service/CommentServiceImp.java
@@ -2,11 +2,13 @@ package com.adam.blog.service;
 
 import com.adam.blog.dao.CommentRepository;
 import com.adam.blog.entity.Comment;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -16,10 +18,14 @@ public class CommentServiceImp implements CommentService {
     @Autowired
     private CommentRepository commentRepository;
 
+    //存放迭代找出的所有子代的集合
+    private List<Comment> tempReplys = new ArrayList<>();
+
     @Override
     public List<Comment> listCommentByBlogId(Long blogId) {
         Sort sort = new Sort(Sort.Direction.DESC, "createTime");
-        return commentRepository.findByBlogId(blogId, sort);
+        List<Comment> comments = commentRepository.findByBlogIdAndParentCommentNull(blogId, sort);
+        return eachComment(comments);
     }
 
     @Transactional
@@ -32,9 +38,63 @@ public class CommentServiceImp implements CommentService {
             comment.setParentComment(null);
         }
         comment.setCreateTime(new Date());
-        System.out.println(comment.toString());
-        Comment s = commentRepository.save(comment);
-        System.out.println("保存成功！");
-        return s;
+        return commentRepository.save(comment);
+    }
+
+
+    /**
+     * 循环每个顶级的评论节点
+     * @param comments
+     * @return
+     */
+    private List<Comment> eachComment(List<Comment> comments) {
+        List<Comment> commentsView = new ArrayList<>();
+        for (Comment comment : comments) {
+            Comment c = new Comment();
+            BeanUtils.copyProperties(comment,c);
+            commentsView.add(c);
+        }
+        //合并评论的各层子代到第一级子代集合中
+        combineChildren(commentsView);
+        return commentsView;
+    }
+
+    /**
+     *
+     * @param comments root根节点，blog不为空的对象集合
+     * @return
+     */
+    private void combineChildren(List<Comment> comments) {
+
+        for (Comment comment : comments) {
+            List<Comment> replys = comment.getReplyComments();
+            for(Comment reply : replys) {
+                //循环迭代，找出子代，存放在tempReplys中
+                recursively(reply);
+            }
+            //修改顶级节点的reply集合为迭代处理后的集合
+            comment.setReplyComments(tempReplys);
+            //清除临时存放区
+            tempReplys = new ArrayList<>();
+        }
+    }
+
+
+    /**
+     * 递归迭代，剥洋葱
+     * @param comment 被迭代的对象
+     * @return
+     */
+    private void recursively(Comment comment) {
+        tempReplys.add(comment);//顶节点添加到临时存放集合
+        if (comment.getReplyComments().size()>0) {
+            List<Comment> replys = comment.getReplyComments();
+            for (Comment reply : replys) {
+                tempReplys.add(reply);
+                if (reply.getReplyComments().size()>0) {
+                    recursively(reply);
+                }
+            }
+        }
     }
 }
