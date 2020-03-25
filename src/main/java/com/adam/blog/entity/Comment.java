d40d55f55194328e8fa1cbf9b8e1b6cb7355e25a
adamboov <48423629+adamboov@users.noreply.github.com>
2020-03-25 15:15:51 +0800

    500 error 模板修复

--- src/main/java/com/adam/blog/entity/Comment.java
+++ src/main/java/com/adam/blog/entity/Comment.java
@@ -28,6 +28,16 @@ public class Comment {
     @ManyToOne
     private Comment parentComment;
 
+    private boolean adminComment;
+
+    public boolean isAdminComment() {
+        return adminComment;
+    }
+
+    public void setAdminComment(boolean adminComment) {
+        this.adminComment = adminComment;
+    }
+
     public Comment() {
     }
 
@@ -112,6 +122,10 @@ public class Comment {
                 ", content='" + content + '\'' +
                 ", avatar='" + avatar + '\'' +
                 ", createTime=" + createTime +
+                ", blog=" + blog +
+                ", replyComments=" + replyComments +
+                ", parentComment=" + parentComment +
+                ", adminComment=" + adminComment +
                 '}';
     }
 }
